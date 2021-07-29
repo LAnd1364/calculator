225d4c1a9eec8dd036c404eabdda280340fa2c35
LAnd1364 <and.lazarenko@gmail.com>
2021-07-29 17:21:31 +0300

    calculator

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+VanillaTilt.init(document.querySelector(".container"), {
+  max: 20,
+  // scale: 1.3,
+  speed: 800,
+  glare: true,
+  "max-glare": .05, 
+
+
+});
+
+//It also supports NodeList
+// VanillaTilt.init(document.querySelectorAll(".your-element"));
